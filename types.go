2c592d6c7dabae0aa4cdfc8ae6d66222eb04c34a
Julien Garcia Gonzalez <julien@giantswarm.io>
2017-12-18 13:27:48 +0100

    add random Key Searcher
    Signed-off-by: Julien Garcia Gonzalez <julien@giantswarm.io>

---
+++ types.go
@@ -0,0 +1,32 @@
+package randomkeys
+
+// These constants are used when filtering the secrets, to only retrieve the
+// ones we are interested in.
+const (
+	// componentLabel is the label used in the secret to identify a secret
+	// containing the random key
+	//
+	// TODO replace with "giantswarm.io/randomkey" and add to
+	// https://github.com/giantswarm/fmt.
+	randomkeyLabel = "clusterKey"
+	// clusterIDLabel is the label used in the secret to identify a secret
+	// containing the random key.
+	//
+	// TODO replace with "giantswarm.io/cluster-id"
+	clusterIDLabel = "clusterID"
+
+	SecretNamespace = "default"
+)
+
+type key string
+type RandomKey []byte
+
+// These constants used as RandomKey
+// parsing a secret received from the API.
+const (
+	encryptionKey key = "encryption"
+)
+
+type Cluster struct {
+	APIServerEncryptionKey RandomKey
+}
