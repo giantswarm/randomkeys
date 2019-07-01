ab46c09c45aab33c03ece6602c08857e02163728
Ross Fairbanks <ross@rossfairbanks.com>
2019-07-01 07:54:19 +0200

    Compiling

--- searcher.go
+++ searcher.go
@@ -85,7 +85,7 @@ func (s *Searcher) search(randomKey *RandomKey, clusterID string, key Key) error
 	// cluster clusterID.
 	selector := fmt.Sprintf("%s=%s, %s=%s", legacyRandomKeyLabel, key, legacyClusterIDLabel, clusterID)
 
-	watcher, err := s.k8sClient.Core().Secrets(SecretNamespace).Watch(metav1.ListOptions{
+	watcher, err := s.k8sClient.CoreV1().Secrets(SecretNamespace).Watch(metav1.ListOptions{
 		LabelSelector: selector,
 	})
 	if err != nil {
